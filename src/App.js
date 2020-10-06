d8225d47fe96a0e4e810f6acf8e734bb113e0478
Umer <umerraja@live.co.uk>
2020-10-06 18:10:34 +0100

    Responsive Mobile Design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import Navigation from './components/navigation';
 import Footer from '../src/components/props/footer';
+import { useMediaQuery, Context as ResponsiveContext } from 'react-responsive'
 import '../src/css/App.css';
 import '../src/css/Navigation.css';
 
+
 function App() {
+
+  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
+  const isBigScreen = useMediaQuery({ minDeviceWidth: 1824 })
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 500 })
+  const isPortrait = useMediaQuery({ orientation: 'portrait' })
+  const isRetina = useMediaQuery({ minResolution: '2dppx' })
+
   return (
+      
     <div id = "App">
-        <Navigation/>
-        <Footer/>
+      { isDesktopOrLaptop &&
+        <ResponsiveContext.Provider value={{ width: 1200 }}>
+          <Navigation/>
+          <Footer/>
+        </ResponsiveContext.Provider>
+      }
+      { isTabletOrMobileDevice &&
+        <ResponsiveContext.Provider value={{ width: 500 }}>
+          <Navigation/>
+          <Footer/>
+        </ResponsiveContext.Provider>
+      }
     </div>
   );
 }
