9870e08d77d24e9e38d6bcd42a1790fefaeafd7b
Umer <umerraja@live.co.uk>
2020-10-06 17:36:30 +0100

    Go To HomePage when HeaderImage click

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -4,7 +4,7 @@ import Header from '../props/header';
 function Main(props) {
   return (
     <div className = "main">
-      <Header name = "Home page"/>
+      <Header name = "Home"/>
       <h1>
         Hi, my name is Umer
         Welcome to my Portfolio, 
--- src/components/props/header.js
+++ src/components/props/header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Logo from '../../images/logo.jpg'
+import { useHistory } from 'react-router-dom';
 import '../../css/App.css'
 
 function Header(props) {
+  const history = useHistory();
+  const GoToHomePage = () => history.push('/');
+
   return (
     <div className = "App-header">
-      <img src = {Logo} className = "circle-icon"/>
+      <img src = {Logo} className = "circle-icon" alt ="logo" onClick={GoToHomePage}/>
         <header>
             <h1>{props.name}</h1>
         </header>
